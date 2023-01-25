d768d992cbe809959896c485c4ed4afc003a9a44
eleno <e.lenoksls@gmail.com>
2023-01-25 16:58:37 +0100

    SaleBlock at MainPage added

--- src/components/MainPageBlocks/SaleBlock/index.jsx
+++ src/components/MainPageBlocks/SaleBlock/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import s from './index.module.css';
+import sale_image from './data/sale_image.svg'
 
 export default function SaleBlock() {
   return (
-    <section className={s.sale_block}>SaleBlock</section>
+    <section className={s.sale_block}>
+      <div className={['content_wrapper', s.content].join(' ')}>
+        <div className={s.info}>
+          <h2>Sale</h2>
+          <h3>New season sale</h3>
+          <div className={s.btn_container}>
+            <button>All offers</button>
+            <button>See more</button>
+          </div>
+        </div>
+        <div className={s.img}>
+          <img src={sale_image} alt="Picture" />
+        </div>
+      </div>
+    </section>
   )
 }
